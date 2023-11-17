8ebd9630c910bfa2ebef1624a50f9f6b2d6377ab
Afranio C <afraniomcaires@gmail.com>
2023-11-17 20:16:24 -0300

    inclusão de try catch

--- Modulo-02/Aula-05/main.js
+++ Modulo-02/Aula-05/main.js
@@ -1,23 +1,27 @@
-let peso = prompt("Digite o seu peso (kg)");
-let altura = prompt("Digite a sua altura (m):");
+try {
+  let peso = prompt("Digite o seu peso (kg)");
+  let altura = prompt("Digite a sua altura (m):");
 
-peso = parseFloat(peso);
-altura = parseFloat(altura);
+  peso = parseFloat(peso);
+  altura = parseFloat(altura);
 
-if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
-  alert("Por favor, insira valores válidos.");
-} else {
-  let imc = peso / (altura * altura);
-  let resultado = "Seu IMC é: " + imc.toFixed(2) + "\n";
-
-  if (imc < 18.5) {
-    resultado += "Abaixo do peso";
-  } else if (imc >= 18.5 && imc < 24.9) {
-    resultado += "Peso normal";
-  } else if (imc >= 25 && imc < 29.9) {
-    resultado += "Sobrepeso";
+  if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+    throw new Error("Por favor, insira valores válidos.");
   } else {
-    resultado += "Obesidade";
+    let imc = peso / (altura * altura);
+    let resultado = "Seu IMC é: " + imc.toFixed(2) + "\n";
+
+    if (imc < 18.5) {
+      resultado += "Abaixo do peso";
+    } else if (imc >= 18.5 && imc < 24.9) {
+      resultado += "Peso normal";
+    } else if (imc >= 25 && imc < 29.9) {
+      resultado += "Sobrepeso";
+    } else {
+      resultado += "Obesidade";
+    }
+    alert(resultado);
   }
-  alert(resultado);
+} catch (error) {
+  alert(error.message);
 }
